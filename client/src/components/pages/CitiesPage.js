deaec264b999ce60abd0f16ed524726dd394b68d
Carlos Barata <baratac@gmail.com>
2019-12-19 18:00:30 +0000

    Introducing Selection Bar

--- client/src/components/pages/CitiesPage.js
+++ client/src/components/pages/CitiesPage.js
@@ -5,16 +5,35 @@ import './pages.css'
 
 function CitiesPage(props) {
     const [cityList, setCityList] = useState([])
-    //const [srchCity, setSrchCity] = useState('')
+    const [srchCity, setSrchCity] = useState('')
 
     useEffect(() => {
         setCityList(props.theList)
     }, [props.theList]);
 
+    const updateCityList = (event) => {
+      const srchString = event.target.value
+      let newList = props.theList;
+
+      setSrchCity(srchString)
+      if (srchString.length > 0) {
+          newList = props.theList.filter(item => {
+          return item.name.toUpperCase().startsWith(srchString.toUpperCase())
+        })
+      }
+      setCityList(newList)
+      //console.log("SRCH: ", srchString, newList )
+    }
+    
     //console.log('Cities List: ', elemList)
     return (
         <div className="city-list">
-            
+            <input 
+              type="text" 
+              name="citySearch"
+              placeholder="Search City"
+              value={srchCity}
+              onChange={updateCityList}/>
              {cityList.map((item) => (<CityEntry item={item} key = { item._id } />))}
         </div>
     )
